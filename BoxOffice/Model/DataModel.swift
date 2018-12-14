ed0a49490d8852a4afff0a23444094bdec0c9d05
kimhyeri <hyer1k@naver.com>
2018-12-15 02:59:49 +0900

    set url session data

--- BoxOffice/Model/DataModel.swift
+++ BoxOffice/Model/DataModel.swift
@@ -12,22 +12,15 @@ struct Movies: Codable {
     
     let date: String
     let grade: Int
-    let id: Int
+    let id: String
+    let reservation_grade: Int
+    let reservation_rate: Double
     let thumb: String
     let title: String
-    let user_rating: String
-
-    init(date: String, grade: Int, id: Int, thumb: String, title: String, user_rating: String) {
-        self.date = date
-        self.grade = grade
-        self.id = id
-        self.thumb = thumb
-        self.title = title
-        self.user_rating = user_rating
-    }
+    let user_rating: Double
     
 }
 
 struct APIResponse: Codable {
-    let movieList: [Movies]
+    let movies: [Movies]
 }
